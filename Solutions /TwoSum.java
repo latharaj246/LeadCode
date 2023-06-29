6e2c8b69c0e2c33839e64ce4dd83bac58d7a92c3
Latha <latha.r@mpokket.com>
2023-06-30 00:28:55 +0530

    twosum for loops

---
+++ Solutions /TwoSum.java
@@ -0,0 +1,26 @@
+/**
+ * TwoSum
+ */
+
+
+ public class TwoSum {
+        public static void main(String[] args) {
+        TwoSum twoSum = new TwoSum();
+        int traget = twoSum.calculateSum( 4, 9);
+        System.out.println("Sum: " + traget);
+    }
+
+    public int calculateSum(int nums, int target) {
+        int i;
+        int j;
+        
+        for (i=0;i<=.length + 1;i++ ){
+            for (j=0; j<=.length + 1; j++){
+             
+
+            }
+        }
+        
+        return traget;
+    }
+}
